1e23a01705b500e78099ec8305cf3b6c1364440c
NoamLeFantom <noamlgfc4@gmail.com>
2025-01-08 19:22:54 +0100

    Patch skillsMap
    maj

--- components/SkillsMap.tsx
+++ components/SkillsMap.tsx
@@ -1,9 +1,34 @@
 import React, { useEffect, useRef } from "react";
 import { createForceGraph } from "../components/skillsMapJS.js";
 
+type Node = {
+  id: string;
+  group: number;
+};
 
-const ForceGraph = ({ data, legend }) => {
-  const containerRef = useRef(null);
+type Link = {
+  source: string | Node;
+  target: string | Node;
+  value?: number;
+};
+
+type SkillsData = {
+  nodes: Node[];
+  links: Link[];
+};
+
+type LegendItem = {
+  color: string;
+  label: string;
+};
+
+type ForceGraphProps = {
+  data: SkillsData;
+  legend: LegendItem[];
+};
+
+const ForceGraph: React.FC<ForceGraphProps> = ({ data, legend }) => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     if (containerRef.current) {
