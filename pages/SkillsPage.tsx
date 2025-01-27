2625bda54125d913de497f5442c16fe8e4eba470
NoamLeFantom <noamlgfc4@gmail.com>
2025-01-27 23:00:03 +0100

    MajPatch ForceGraph

--- pages/SkillsPage.tsx
+++ pages/SkillsPage.tsx
@@ -1,23 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ForceGraph from "../components/SkillsMap";
 import Header from "../components/Header";
 import skillsData from "../public/src/data/skills.json";
 
 const legendData = [
-    { color: "#1f77b4", label: "Entreprendre" },
-    { color: "#ff7f0e", label: "Comprendre" },
-    { color: "#2ca02c", label: "Développer" },
-    { color: "#D62728", label: "Concevoir" },
-    { color: "#9467BD", label: "Exprimer" },
-  ];
+  { color: "#1f77b4", label: "Entreprendre" },
+  { color: "#ff7f0e", label: "Comprendre" },
+  { color: "#2ca02c", label: "Développer" },
+  { color: "#D62728", label: "Concevoir" },
+  { color: "#9467BD", label: "Exprimer" },
+];
 
-const App = () => {
-    return (
-        <div className={"GlobalPage"}>
-            <Header BackgroundFill={"#E0694B"}/>
-            <ForceGraph data={skillsData} legend={legendData} />
-        </div>
-    );
+const SkillsPage = () => {
+  const [resourceDetails, setResourceDetails] = useState<any | null>(null);
+
+  useEffect(() => {
+    fetch("/src/ficheMemoire/data/ficheMemoire.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erreur lors du chargement du JSON.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setResourceDetails(data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div className={"GlobalPage"}>
+      <Header BackgroundFill={"#E0694B"} />
+      {resourceDetails && (
+        <ForceGraph
+          data={skillsData}
+          legend={legendData}
+          resourceDetails={resourceDetails} // Passer les détails ici
+        />
+      )}
+    </div>
+  );
 };
 
-export default App;
+export default SkillsPage;
